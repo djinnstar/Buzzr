a2680821e43fc9f6cbc3a08df2cbf4070afa77db
DjinnStar <jensenpj85@gmail.com>
2023-12-25 17:03:59 -0600

    Add project files.

---
+++ Buzzer/Buzzer/BuzzData.cs
@@ -0,0 +1,53 @@
+﻿namespace Buzzer;
+
+public class BuzzData
+{
+    public string HostConnectionID = "";
+    public List<(string Name, long Time)> Buzzes { get; set; } = new();
+
+    public void Add(string name, long time)
+    {
+        Buzzes.Add((name, time));
+        Buzzes.Sort((pair1, pair2) => pair1.Time.CompareTo(pair2.Time));
+    }
+
+    public string Serialize()
+    {
+        return HostConnectionID + Buzzes.Select(pair => pair.Name + " @ " + pair.Time.ToString()).Aggregate("", (output, input) => output += "|||" + input);
+    }
+
+    public static BuzzData Deserialize(string encoded) 
+    {
+        BuzzData data = new BuzzData();
+        data.HostConnectionID = encoded.Split("|||")[0];
+        if (encoded.Split("|||").Length > 1)
+        {
+            var buzzes = encoded.Split("|||");
+            for (int i = 1; i < buzzes.Length; i++)
+            {
+                var split = buzzes[i].Split(" @ ");
+                data.Add(split[0], long.Parse(split[1]));
+            }
+        }
+        return data;
+    }
+
+    public static BuzzData LoadOrCreate()
+    {
+        if (File.Exists("data.txt"))
+        {
+            return Deserialize(File.ReadAllText("data.txt"));
+        }
+        else
+        {
+            var output = new BuzzData();
+            output.Save();
+            return output;
+        }
+    }
+
+    public void Save()
+    {
+        File.WriteAllText("data.txt", Serialize());
+    }
+}
---
+++ Buzzer/Buzzer/MyHub.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Buzzer.Hubs;
+
+public class MyHub : Hub
+{
+    public async Task UpdateGame()
+    {
+        await Clients.All.SendAsync("UpdateData", BuzzData.LoadOrCreate().Serialize());
+    }
+
+    public async Task Buzz(string name, string dateTimeEncoded)
+    {
+        BuzzData data = BuzzData.LoadOrCreate();
+        data.Add(name, long.Parse(dateTimeEncoded));
+        data.Save();
+        await Clients.All.SendAsync("UpdateData", data.Serialize());
+    }
+
+    public async Task Clear()
+    {
+        BuzzData data = BuzzData.LoadOrCreate();
+        data.Buzzes = new();
+        data.Save();
+        await Clients.All.SendAsync("UpdateData", data.Serialize());
+    }
+
+    public async Task JoinAsHost(string name)
+    {
+        BuzzData data = BuzzData.LoadOrCreate();
+        if (data.HostConnectionID == "")
+        {
+            data.HostConnectionID = Context.ConnectionId;
+            data.Save();
+            await Clients.All.SendAsync("UpdateData", data.Serialize());
+        }
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        BuzzData data = BuzzData.LoadOrCreate();
+        if (Context.ConnectionId == data.HostConnectionID)
+        {
+            data.HostConnectionID = "";
+            data.Save();
+            await Clients.All.SendAsync("UpdateData", data.Serialize());
+        }
+        await base.OnDisconnectedAsync(exception);
+    }
+}
